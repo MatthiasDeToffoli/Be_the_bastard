c0a9668cdf8492f2077f372acf4c35a3a8bfd288
Matthias de Toffoli <matew83@hotmail.fr>
2017-04-12 15:04:23 +0200

    Add : animation walk when player move

--- Assets/Scripts/GameObjects/Player/PlayerAgent.cs
+++ Assets/Scripts/GameObjects/Player/PlayerAgent.cs
@@ -14,10 +14,11 @@ namespace Assets.Scripts.GameObjects.Player
         private static PlayerAgent _instance;
         private NavMeshAgent agent;
 
-        private Vector3 previousPosition;
-        private Vector3 actualPosition;
         private GameObject objClicked;
 
+        private Vector3 pos;
+        private Animation anim;
+        private Action doAction;
         /// <summary>
         /// instance unique de la classe     
         /// </summary>
@@ -36,48 +37,73 @@ namespace Assets.Scripts.GameObjects.Player
                 throw new Exception("Tentative de création d'une autre instance de PlayerAgent alors que c'est un singleton.");
             }
             _instance = this;
+
+            pos = Vector3.zero;
         }
 
         protected void Start()
         {
             agent = GetComponent<NavMeshAgent>();
-            actualPosition = transform.position;
-            previousPosition = transform.position;
+
+            anim = GetComponent<Animation>();
+            SetModeVoid();
+            //agent.SetDestination(new Vector3(5, 0, 0));
         }
 
         protected void Update()
         {
-            actualPosition = transform.position;
-            if (actualPosition != previousPosition) {
-                previousPosition = actualPosition;
-            }
-            else {
-                if (objClicked && objClicked.transform.tag != "bubblePlane") {
-                    Vector3 roundTargetPos = new Vector3(objClicked.transform.position.x, 0, objClicked.transform.position.z);
-                    Vector3 roundAgentPos = new Vector3(transform.position.x, 0, transform.position.z);
-                    transform.rotation = Quaternion.LookRotation(roundTargetPos - roundAgentPos);
-                }
-            }
+            doAction();
 
             if (Input.GetMouseButtonDown(0))
             {
-                /* Vector3 pos = Input.mousePosition;
-                 pos.z = transform.position.z;
-                 pos = Camera.main.ScreenToWorldPoint(pos);
-                 Ray ray = new Ray(pos,Vector3.down);*/
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit))
                 {
                     agent.SetDestination(hit.point);
+
                     objClicked = GameObject.Find(hit.transform.name);
                 }          
+
+                    SetModeMove();
+                    
+                }      
+
+         }
+
+        protected void SetModeVoid()
+        {
+            doAction = DoActionVoid;
+        }
+
+        protected void DoActionVoid()
+        {
+
+        }
+
+        protected void DoActionMove()
+        {
+            if (!agent.hasPath)
+            {
+                if(objClicked.transform.tag != "bubblePlane")
+                {
+                    Vector3 roundTargetPos = new Vector3(objClicked.transform.position.x, 0, objClicked.transform.position.z);
+                    Vector3 roundAgentPos = new Vector3(transform.position.x, 0, transform.position.z);
+                    transform.rotation = Quaternion.LookRotation(roundTargetPos - roundAgentPos);
+                }
+                
+                anim.Play("sleep");
+                SetModeVoid();
             }
         }
 
-        protected void checkPlayerMovement()
+        protected void SetModeMove()
         {
+            anim["walk"].speed = 4f;
+            anim.wrapMode = WrapMode.Loop;
+            anim.Play("walk");
 
+            doAction = DoActionMove;
         }
 
         protected void OnDestroy()
