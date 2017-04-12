c7d31500dcd1047905dd8dd5336ea93fa87da18d
Matthias de Toffoli <matew83@hotmail.fr>
2017-04-12 12:08:57 +0200

    Add : animations asset + test animations

---
+++ Assets/Scripts/GameObjects/Player/Anim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GameObjects.Player
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Anim : MonoBehaviour
+    {
+
+        protected void Start()
+        {
+            GetComponent<Animation>().wrapMode = WrapMode.Loop;
+//GetComponent<Animation>().Play("bobble");
+            //GetComponent<Animation>().Play("drink");
+
+            //GetComponent<Animation>().Play("fight");
+            //GetComponent<Animation>().Play("sleep");
+           GetComponent<Animation>().Play("use_computer");
+            //GetComponent<Animation>().Play("walk");
+
+        }
+
+        protected void Update()
+        {
+
+        }
+    }
+}
