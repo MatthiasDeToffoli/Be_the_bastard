efd75b0da2ba45fc475c75c5e59fbc7be2e52647
Matthias de Toffoli <matew83@hotmail.fr>
2017-04-14 10:35:20 +0200

    Add : camera

---
+++ Assets/Scripts/GameObjects/CameraMove.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using Assets.Scripts.GameObjects.Player;
+
+namespace Assets.Scripts.GameObjects
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CameraMove : MonoBehaviour
+    {
+
+        private static CameraMove _instance;
+        protected GameObject player;       //Public variable to store a reference to the player game object
+
+
+        private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+        /// <summary>
+        /// instance unique de la classe     
+        /// </summary>
+        public static CameraMove instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            if (_instance != null)
+            {
+                throw new Exception("Tentative de création d'une autre instance de Camera alors que c'est un singleton.");
+            }
+            _instance = this;
+        }
+
+        protected void Start()
+        {
+            player = PlayerAgent.instance.gameObject;
+            offset = transform.position - player.transform.position;
+        }
+
+        void LateUpdate()
+        {
+            // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+            transform.position = player.transform.position + offset;
+        }
+
+        protected void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
+}
